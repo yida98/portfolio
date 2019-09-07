df8ac3ccba2b99ca2a4048efbe3593799f5fad55
Yida Zhang <yidazhang@Yidas-MacBook-Pro.local>
2019-09-07 14:24:45 -0400

    initial commit

---
+++ myscripts.js
@@ -0,0 +1,36 @@
+var col = document.getElementById("col");
+
+window.onscroll = function() {toggleVisibility()}
+
+function toggleVisibility() {
+    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+        col.style.visibility = "visible";
+        col.classList.add("trans");
+    } else {
+        col.style.visibility = "hidden";
+    }
+};
+
+window.onbeforeunload = function() {
+    window.scrollTo(0, 0);
+}
+
+function changeActivity(event) {
+    var activeElement = document.getElementsByClassName("active")[0];
+    if ((event.target.classList.contains("active")) == false) {
+        activeElement.classList.remove("active");
+        event.target.classList.add("active");
+    }
+}
+
+window.onload = function() {
+    
+    var path = window.location.href;
+    if (path.indexOf("#paintings") > -1){
+        document.getElementById("painter").classList.add("active");
+        document.getElementById("product").classList.remove("active");
+    } else {
+        document.getElementById("product").classList.add("active");
+        document.getElementById("painter").classList.remove("active");
+    }
+}
