467d724dd89a6d87d557f8fe46f2a07673697a04
Yida Zhang <yidazhang@Yidas-MacBook-Pro.local>
2019-09-08 09:40:01 -0400

    transitioning between tabs is a-okay

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,8 @@
 var col = document.getElementById("col");
+var painterEl = document.getElementById("painter");
+var productEl = document.getElementById("product");
+var productSec = document.getElementById("type1");
+var painterSec = document.getElementById("type2");
 
 window.onscroll = function() {toggleVisibility()}
 
@@ -26,17 +30,35 @@ function changeActivity(event) {
     } else {
         document.getElementById("type1").style.display = "block";
         document.getElementById("type2").style.display = "none";
+        hideTransible();
     }
 }
 
 window.onload = function() {
-    
     var path = window.location.href;
     if (path.indexOf("#paintings") > -1){
-        document.getElementById("painter").classList.add("active");
-        document.getElementById("product").classList.remove("active");
+        painterEl.classList.add("active");
+        painterSec.classList.add("trans1")
+        productEl.classList.remove("active");
+        if (productSec.classList.contains("trans1")) {
+            productSec.classList.remove("trans1")
+        }
     } else {
-        document.getElementById("product").classList.add("active");
-        document.getElementById("painter").classList.remove("active");
+        productEl.classList.add("active");
+        hideTransible();
+        productSec.classList.add("trans1")
+        
+        painterEl.classList.remove("active");
+        if (painterSec.classList.contains("trans1")) {
+            painterSec.classList.remove("trans1")
+        }
+    }
+}
+
+function hideTransible() {
+    
+    col.style.visibility = "hidden";
+    if (col.classList.contains("trans")) {
+        col.classList.remove("trans");
     }
 }
